585ff1cadca5ce58f035963d93bc1ba48e7505df
AhmedHoussamBouzine <ahmedhoussam.bouzine.01@gmail.com>
2023-08-22 18:55:47 +0100

    first init

---
+++ index.js
@@ -0,0 +1,33 @@
+const WebSocket = require('ws');
+
+const socketUrl = 'wss://stream.binance.com:9443/ws/!ticker@arr';
+
+const ws = new WebSocket(socketUrl);
+
+ws.on('message', (data) => {
+
+    console.log('WebSocket connection opened');
+
+    try {
+        const coinData = JSON.parse(data);
+
+        coinData.forEach(data => {
+            const coin = {
+                symbol: data.s.replace('USDT', ''),
+                price: data.c,
+                date: new Date()
+            }
+            console.log(coin)
+        });
+    } catch (error) {
+        console.error('Error processing message:', error);
+    }
+});
+
+ws.on('error', (error) => {
+    console.error('WebSocket error', error);
+});
+
+ws.on('close', () => {
+    console.log('WebSocket connection closed');
+});
